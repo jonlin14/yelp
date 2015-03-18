9a1cefc06c12b2b356a853a2c80e09f083cfe496
Jonathan Lin & Kelly deVries <student@epicodus.com>
2015-03-18 11:51:41 -0700

    Add delete button to index page that will not redirct after clicking delete

--- app/app.php
+++ app/app.php
@@ -15,11 +15,21 @@
             return $app['twig']->render('index.html.twig', array('restaurants' => Restaurant::getAll()));
         });
 
+        $app->post("/", function() use ($app){
+            $restaurants = Restaurant::deleteAll();
+            return $app['twig']->render('index.html.twig', array('restaurants' => $restaurants));
+        });
+
         $app->post("/restaurant", function() use ($app) {
             $new_restaurant = new Restaurant($_POST['name']);
             $new_restaurant->save();
             $restaurants = Restaurant::getAll();
 
+            return $app['twig']->render('restaurant.html.twig', array('restaurants' => $restaurants));
+        });
+
+        $app->post("/delete_restaurant", function() use ($app) {
+            $restaurants = Restaurant::deleteAll();
             return $app['twig']->render('restaurant.html.twig', array('restaurants' => $restaurants));
         });
         return $app;
